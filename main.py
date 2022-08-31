c4ef9c50a7e68641f17a55b7d18856c4e953ce4c
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-31 18:26:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
-THIS CODE COPIES ITSELF AND WILL CREATE NEW DIRECTORIES,
-PLEASE MIND THAT THIS WOULD PROBABLY DESTORY (maybe) UR NICE DIRECTORY AND MAKING IT MESSY.
-DO NOT RUN THIS FILE IN ANY IMPORTANT DIRECTORIES
+THIS CODE REPLICATES ITSELF AND WILL CREATE NEW DIRECTORIES
+PLEASE MIND THAT THIS WOULD DESTORY YOUR NICE DIRECTORIES, INCLUDING FILES AND FOLDERS
+***DO NOT RUN THIS FILE IN ANY IMPORTANT DIRECTORIES***
 
 self-replicating python file by archisha69
 '''
@@ -13,15 +13,15 @@ cwd = os.getcwd()
 
 count = 0
 
-while count < 10: #change value to whatever amount of directories you want it to create, beware of this could lag your devices
+while True: #warning: this is an infinity loop without breaking
     name = ''.join(random.choice(string.printable) #ramdom string generator
     time.sleep(1) #cooldown, lower the value to make the process faster, higher the value to make the process slower (prevent lags)
     os.mkdir(f"{cwd}{sep}{name}{count}") #created directory name will be random printable characters
-    count += 1
+    count += 1 #increases count everytime it creates a directory
     
-if count == 10:
+if count == 10: #start self-replicating when count to 10
     all = os.listdir(cwd) #returns a list of all current directories
-    for dir in all:
+    for dir in all: #for every directories and files
         if dir == "main.py": pass #skip existed (this file) main.py in current directory, if you already have 1 file named "main.py" in current working directory (cwd), please change the name of this file and change "main.py" in this line to this file's name after changes
         else:
             time.sleep(1) #cooldown, as same as the previous one
