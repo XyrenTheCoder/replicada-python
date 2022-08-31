02dc458b11e1cc7ecde011e4546e11a6a5eb1703
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-31 13:14:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ import os, shutil, time, random, string
 sep = "\\" if os.name == "nt" else "/"
 cwd = os.getcwd()
 
-name = ''.join(random.choice(string.printable)
-
 count = 0
 
 while count < 10: #change value to whatever amount of directories you want it to create, beware of this could lag your devices
@@ -23,6 +21,7 @@ while count < 10: #change value to whatever amount of directories you want it to
 if count == 10:
     all = os.listdir(cwd) #returns a list of all current directories
     for dir in all:
+        name = ''.join(random.choice(string.printable)
         if dir == "main.py": pass #skip existed (this file) main.py in current directory, if you already have 1 file named "main.py" in current working directory (cwd), please change the name of this file and change "main.py" in this line to this file's name after changes
         else:
             time.sleep(1) #cooldown, as same as the previous one
